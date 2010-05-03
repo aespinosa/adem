9b5b837a1d9284060ff680c8d7cbbe8d07bcb1b5
aespinosa <aespinosa@e2bb083e-7f23-0410-b3a8-8253ac9ef6d8>
2010-05-03 19:56:55 +0000

    Updated RunTest dummy paths
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/SwiftApps/adem-osg@3319 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- test/test_run.rb
+++ test/test_run.rb
@@ -4,13 +4,16 @@ class RunTest < Test::Unit::TestCase
   include Adem::TestSetup
 
   def test_config
-    assert_equal(@conf, run_command(["config"], "config", nil))
+    assert_equal(@conf, run_command(["config"], "test/dummy_config", nil))
   end
 
   def test_sites_from_file
     conf = @conf
     site_list = YAML.load @site_list
     conf[:sites] = site_list
-    assert_equal(conf, run_command(["sites"], "config", "sites"))
+    assert_equal(
+      conf,
+      run_command(["sites"],"test/dummy_config", "test/dummy_sites")
+    )
   end
 end
